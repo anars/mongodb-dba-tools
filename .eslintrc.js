f0abb4d100f7ba8aa86751fc82cf7bb5ff08a7f9
Kay Anar <git@anars.com>
2020-03-08 17:25:10 -0400

    eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+  },
+  "extends": [
+    "google",
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+  },
+  "rules": {
+    "quotes": ["error", "double"],
+    "max-len": ["error", {
+      "code": 160
+    }]
+  },
+};
--- top.js
+++ top.js
@@ -6,25 +6,25 @@ let printVersion = false;
 const mongotopParams = [];
 
 process.argv.forEach((param, index) => {
-    if (index > 1) { // eslint-disable-line no-magic-numbers
-        if (param.toLowerCase().startsWith("--regex=")) {
-            nsRegEx = new RegExp(param.substr(8), "u"); // eslint-disable-line no-magic-numbers
-        } else if (param.toLowerCase().startsWith("--delimiter=")) {
-            delimiter = param.substr(12); // eslint-disable-line no-magic-numbers
-        } else if (param.toLowerCase() === "--help") {
-            printHelp = true;
-        } else if (param.toLowerCase() === "--version") {
-            printVersion = true;
-        } else if (param.toLowerCase() === "--json") {
-            outputFormat = "json";
-        } else if (param.toLowerCase() === "--csv") {
-            outputFormat = "csv";
-        } else if (param.toLowerCase() === "--xml") {
-            outputFormat = "xml";
-        } else if (!param.toLowerCase().startsWith("--verbose") && !param.toLowerCase().startsWith("-v")) {
-            mongotopParams.push(param);
-        }
+  if (index > 1) { // eslint-disable-line no-magic-numbers
+    if (param.toLowerCase().startsWith("--regex=")) {
+      nsRegEx = new RegExp(param.substr(8), "u"); // eslint-disable-line no-magic-numbers
+    } else if (param.toLowerCase().startsWith("--delimiter=")) {
+      delimiter = param.substr(12); // eslint-disable-line no-magic-numbers
+    } else if (param.toLowerCase() === "--help") {
+      printHelp = true;
+    } else if (param.toLowerCase() === "--version") {
+      printVersion = true;
+    } else if (param.toLowerCase() === "--json") {
+      outputFormat = "json";
+    } else if (param.toLowerCase() === "--csv") {
+      outputFormat = "csv";
+    } else if (param.toLowerCase() === "--xml") {
+      outputFormat = "xml";
+    } else if (!param.toLowerCase().startsWith("--verbose") && !param.toLowerCase().startsWith("-v")) {
+      mongotopParams.push(param);
     }
+  }
 });
 
 if (printHelp) {
@@ -37,54 +37,52 @@ let printHeader = true;
 const keys = [];
 
 const dumpCSV = function dumpCSV(json) {
-    if (printHeader) {
-        keys.length = 0;
-        const headers = ["timestamp"];
-        for (const header in json.totals) {
-            if (nsRegEx.test(header) && Reflect.has(json.totals, header)) {
-                keys.push(header);
-                headers.push(`${header} (total time)`);
-                headers.push(`${header} (total count)`);
-                headers.push(`${header} (read time)`);
-                headers.push(`${header} (read count)`);
-                headers.push(`${header} (write time)`);
-                headers.push(`${header} (write count)`);
-            }
-        }
-        console.log(headers.join(delimiter)); // eslint-disable-line no-console
-        printHeader = false;
+  if (printHeader) {
+    keys.length = 0;
+    const headers = ["timestamp"];
+    for (const header in json.totals) {
+      if (nsRegEx.test(header) && Reflect.has(json.totals, header)) {
+        keys.push(header);
+        headers.push(`${header} (total time)`);
+        headers.push(`${header} (total count)`);
+        headers.push(`${header} (read time)`);
+        headers.push(`${header} (read count)`);
+        headers.push(`${header} (write time)`);
+        headers.push(`${header} (write count)`);
+      }
     }
-    const output = [`${json.time}`];
-    keys.forEach((key) => {
-        output.push(json.totals[key].total.time);
-        output.push(json.totals[key].total.count);
-        output.push(json.totals[key].read.time);
-        output.push(json.totals[key].read.count);
-        output.push(json.totals[key].write.time);
-        output.push(json.totals[key].write.count);
-    });
-    console.log(output.join(delimiter)); // eslint-disable-line no-console
+    console.log(headers.join(delimiter)); // eslint-disable-line no-console
+    printHeader = false;
+  }
+  const output = [`${json.time}`];
+  keys.forEach((key) => {
+    output.push(json.totals[key].total.time);
+    output.push(json.totals[key].total.count);
+    output.push(json.totals[key].read.time);
+    output.push(json.totals[key].read.count);
+    output.push(json.totals[key].write.time);
+    output.push(json.totals[key].write.count);
+  });
+  console.log(output.join(delimiter)); // eslint-disable-line no-console
 };
 
 const {spawn} = require("child_process");
 const mongotop = spawn("mongotop", [
-    ...mongotopParams,
-    "--quiet",
-    "--json"
-]);
+  ...mongotopParams,
+  "--quiet",
+  "--json"]);
 
 mongotop.stdout.on("data", (data) => {
-    const json = JSON.parse(data);
-    if (outputFormat === "csv") {
-        dumpCSV(json);
-    }
+  const json = JSON.parse(data);
+  if (outputFormat === "csv") {
+    dumpCSV(json);
+  }
 });
 
 mongotop.stderr.on("data", console.error); // eslint-disable-line no-console
 
 mongotop.on("close", (code) => {
-    if (code !== 0) { // eslint-disable-line no-magic-numbers
-        console.error(`Error : ${code}`); // eslint-disable-line no-console
-    }
+  if (code !== 0) { // eslint-disable-line no-magic-numbers
+    console.error(`Error : ${code}`); // eslint-disable-line no-console
+  }
 });
-
